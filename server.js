187678eda6dd122ee695724ba8c9cb4548f13299
sara <sara.heijkenskjold@cloudist.se>
2023-08-04 11:05:27 +0200

    Add parameterized routes

--- server.js
+++ server.js
@@ -4,11 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
+const friends = [
+  {
     id: 0,
     name: 'Ada Lovelace',
-  });
+  },
+  {
+    id: 1,
+    name: 'Albert Einstein',
+  },
+  {
+    id: 2,
+    name: 'Isaac Newton',
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: 'Friend does not exist',
+    });
+  }
 });
 
 app.get('/messages', (req, res) => {
